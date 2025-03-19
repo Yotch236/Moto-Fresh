47eebe5a20b454f4fc38eb6d5458ea68b08cc015
Yotch236 <gerodayos23@gmail.com>
2025-03-19 18:28:48 +0800

    On Hold

--- script.js
+++ script.js
@@ -23,3 +23,13 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 };
+
+const sr = ScrollReveal({
+    distance: '70px',
+    duration: 2700,
+    reset: true
+});
+
+sr.reveal('.hero-text', {delay: 200, origin: 'bottom'});
+sr.reveal('.hero-img', {delay: 350, origin: 'top'});
+sr.reveal('.down-arrow', {delay: 450, origin: 'right'});
