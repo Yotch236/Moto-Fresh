b39edb23acbb8f867e40cbbd144b039df360351b
Yotch236 <gerodayos23@gmail.com>
2025-03-28 19:42:19 +0800

    moon and sun

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const body = document.querySelector("body"),
       nav = document.querySelector("nav"),
+      modeToggle = document.querySelector(".dark-light"),
       sidebarOpen = document.querySelector(".sidebarOpen"),
       sidebarClose = document.querySelector(".sidebarClose");
+
+      let getMode = localStoraage.getItem("mode");
+         if (getMode && getMode === "dark-mode"){
+            body.classList.add("dark");
+         }
+
+        modeToggle.addEventListener("click", () =>{
+            modeToggle.classList.toggle("active");
+            body.classList.toggle("dark");
+            
+            if(!body.classList.contains("dark")){
+                localStorage.setItem("mode", "light-mode");
+            }else{
+                localStorage.setItem("mode", "dark-mode");
+            }
+        });
 
 sidebarOpen.addEventListener('click', () => {
     nav.classList.add('active');
